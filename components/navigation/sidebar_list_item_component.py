5fbebe23bb839c64ccc2405f1b171032db06de6a
amitabha-i-am <artemushka2000asd@gmail.com>
2026-05-20 01:16:16 +0300

    Настройки автотестов с Pydantic

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -17,7 +17,7 @@ class SidebarListItemComponent(BaseComponent):
         self.title = Text(page, f'{identifier}-drawer-list-item-title-text', 'Navigation title')
         self.button = Button(page, f'{identifier}-drawer-list-item-button', 'Navigation button')
 
-    @allure.step('Check visible "{title}" sidebar list item')
+    @allure.step('Check visibility "{title}" sidebar list item')
     def check_visible(self, title: str):
         self.icon.check_visible()
 
