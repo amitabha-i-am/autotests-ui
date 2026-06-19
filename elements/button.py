570d8929850284cecdfef5591e73d05692584b28
amitabha-i-am <artemushka2000asd@gmail.com>
2026-06-19 20:08:29 +0300

    задание

--- elements/button.py
+++ elements/button.py
@@ -1,5 +1,6 @@
 import allure  # Импортируем allure
 from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger  # Импортируем get_logger
@@ -18,6 +19,8 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)  # Добавили логирование
             expect(locator).to_be_enabled()
+            # Трекаем проверку состояния как ENABLED
+            self.track_coverage(ActionType.ENABLED, nth, **kwargs)
 
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
@@ -25,3 +28,5 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)  # Добавили логирование
             expect(locator).to_be_disabled()
+            # Трекаем проверку состояния как DISABLED
+            self.track_coverage(ActionType.DISABLED, nth, **kwargs)
